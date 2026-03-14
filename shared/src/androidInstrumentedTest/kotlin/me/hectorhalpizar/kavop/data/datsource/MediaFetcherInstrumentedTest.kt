6f7d544c92420315e758cf1ddd99f00345e0389d
hectorhalpizar <hector.alpizar.c@gmail.com>
2026-03-13 23:19:21 -0700

    [DATASOURCE] - load_audio_on_sd_card_music_path.

--- shared/src/androidInstrumentedTest/kotlin/me/hectorhalpizar/kavop/data/datsource/MediaFetcherInstrumentedTest.kt
+++ shared/src/androidInstrumentedTest/kotlin/me/hectorhalpizar/kavop/data/datsource/MediaFetcherInstrumentedTest.kt
@@ -1,28 +1,64 @@
 package me.hectorhalpizar.kavop.data.datsource
 
-import android.util.Log
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.rule.GrantPermissionRule
+import kotlinx.coroutines.test.runTest
 import me.hectorhalpizar.kavop.ApplicationContextHolder
 import me.hectorhalpizar.kavop.data.datasource.getMediaFetcher
-import org.junit.Assert.assertNotEquals
+import me.hectorhalpizar.kavop.shared.test.ui.SelectFileActivityBaseTest
+import me.hectorhalpizar.kavop.util.AndroidMediaPath
+import me.hectorhalpizar.kavop.util.EspressoIdlingUtil
+import me.hectorhalpizar.kavop.util.createMediaPath
+import org.junit.After
+import org.junit.Before
+import org.junit.Ignore
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.test.assertEquals
 
 @RunWith(AndroidJUnit4::class)
-class MediaFetcherInstrumentedTest {
+@Ignore("MediaFetcher verifications are on LoadAudioUseCaseInstrumentedTest.")
+class MediaFetcherInstrumentedTest: SelectFileActivityBaseTest() {
+
+    @get:Rule
+    val permissionRule: GrantPermissionRule =
+        GrantPermissionRule.grant(android.Manifest.permission.READ_EXTERNAL_STORAGE)
+
+    @Before
+    fun setup() {
+        idlingUtil = EspressoIdlingUtil
+        idlingUtil?.register()
+    }
+
+    @After
+    fun tearDown() {
+        idlingUtil?.unregister()
+    }
 
     @Test
-    fun fetchAudio_with_media_on_device() {
+    fun load_audio_on_sd_card_music_path() {
         // Given
-        ApplicationContextHolder.context = InstrumentationRegistry.getInstrumentation().context.applicationContext
-        val testInstance = getMediaFetcher()
+        ApplicationContextHolder.context =
+            InstrumentationRegistry.getInstrumentation().context.applicationContext
 
-        // When
-        val result = testInstance.fetchAudio()
+        folderPickerCallback = { dir ->
+            // Given
+            val testInstance = getMediaFetcher()
+            val mediaPath = createMediaPath() as AndroidMediaPath
+            dir?.let { dir ->
+                mediaPath.path = dir
+            }
 
-        // Then
-        assertNotEquals(0, result.size)
+            runTest {
+                // When
+                val result = testInstance.loadAudio(mediaPath)
+
+                // Then
+                assertEquals(15, result.size, "===> Amount of songs ${result.size}")
+            }
+        }
     }
 
     companion object {
