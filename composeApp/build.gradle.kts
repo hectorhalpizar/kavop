455aecaa82d1b3af01ffc0fbd6c78c897e425ee3
hectorhalpizar <hector.alpizar.c@gmail.com>
2026-03-15 21:17:30 -0700

    [GRADLE] - navigation-compose dependency added.

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -62,6 +62,9 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+    dependencies {
+        implementation("androidx.navigation:navigation-compose:2.7.2")
+    }
 }
 
 dependencies {
