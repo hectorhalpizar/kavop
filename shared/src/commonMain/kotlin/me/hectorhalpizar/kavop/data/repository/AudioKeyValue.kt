08beac159a74d1ac480eddd8189fc102c62e4102
hectorhalpizar <hector.alpizar.c@gmail.com>
2026-03-16 05:59:58 -0700

    [DATA] - AudioKeyValue for constant values added.

---
+++ shared/src/commonMain/kotlin/me/hectorhalpizar/kavop/data/repository/AudioKeyValue.kt
@@ -0,0 +1,9 @@
+package me.hectorhalpizar.kavop.data.repository
+
+/**
+ * Only constants for Key-Value storage must be list in here.
+ */
+internal object AudioKeyValue {
+    const val KAVOP_AUDIO_PLAYER_PREFERENCES = "KAVOP_AUDIO_PLAYER"
+    const val AUDIO_PATHS_KEY = "AUDIO_PATHS"
+}
