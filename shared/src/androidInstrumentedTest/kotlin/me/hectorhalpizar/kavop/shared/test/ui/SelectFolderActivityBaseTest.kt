e7734cbc178844ecd176983af7791168140a44df
hectorhalpizar <hector.alpizar.c@gmail.com>
2026-04-04 00:34:20 -0700

    [TEST] - DocumentFileTestActivityBaseTest added.

---
+++ shared/src/androidInstrumentedTest/kotlin/me/hectorhalpizar/kavop/shared/test/ui/SelectFolderActivityBaseTest.kt
@@ -0,0 +1,61 @@
+package me.hectorhalpizar.kavop.shared.test.ui
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.documentfile.provider.DocumentFile
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+import org.hamcrest.Description
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.hamcrest.TypeSafeMatcher
+import org.junit.Rule
+
+abstract class SelectFolderActivityBaseTest {
+
+    @Rule
+    @JvmField
+    var mActivityScenarioRule = ActivityScenarioRule(SelectFolderActivity::class.java)
+
+    protected var callback : (DocumentFile?) -> Unit = {}
+
+    protected fun selectMusicFolder() {
+
+        val materialButton = onView(
+            allOf(
+                withText("Select folder"),
+                childAtPosition(
+                    childAtPosition(
+                        withId(android.R.id.content),
+                        0
+                    ),
+                    2
+                ),
+                isDisplayed()
+            )
+        )
+        materialButton.perform(click())
+    }
+
+    private fun childAtPosition(
+        parentMatcher: Matcher<View>, position: Int
+    ): Matcher<View> {
+
+        return object : TypeSafeMatcher<View>() {
+            override fun describeTo(description: Description) {
+                description.appendText("Child at position $position in parent ")
+                parentMatcher.describeTo(description)
+            }
+
+            public override fun matchesSafely(view: View): Boolean {
+                val parent = view.parent
+                return parent is ViewGroup && parentMatcher.matches(parent)
+                        && view == parent.getChildAt(position)
+            }
+        }
+    }
+}
